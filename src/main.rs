417da4064d0aba2ac1e9af40fee57274ca9671ea
Wiktoria Janas <wiktoria.janas@pm.me>
2023-08-27 16:25:25 +0200

    Listen on all interfaces by default

--- src/main.rs
+++ src/main.rs
@@ -42,7 +42,7 @@ lazy_static! {
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| App::new().service(rss_rewrite))
-        .bind(("127.0.0.1", 8080))?
+        .bind(("0.0.0.0", 8080))?
         .run()
         .await
 }
